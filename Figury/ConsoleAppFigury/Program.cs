7ea4cc9726f71d00ea5d644aa2c5db39b0ef594a
zsalach <61181802+zsalach@users.noreply.github.com>
2020-03-02 18:51:58 +0100

    2020.03.02
    sdf

---
+++ Figury/ConsoleAppFigury/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FiguryArray;
+
+namespace ConsoleAppFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Trojkat t1;
+            t1 = new Trojkat();
+            Trojkat t2 = new Trojkat(1, 2, 3);
+            var t3 = new Trojkat(c: 1, b: 2, a: 2);
+            //Trojkat t3 = new Trojkat(c: 1, b: 2, a: 2);
+            Trojkat t4 = new Trojkat();
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Figury/FiguryArray/Trojkat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FiguryArray
+{
+    public class Trojkat
+    {
+        // 1.  dane obiektu - pola obiektu (fields)
+
+        double a;       //this.a
+        double b;       //this.b
+        double c;       //this.c
+
+        // 2. konstruktory 
+
+        public Trojkat()            //konstruktor domyślny nie posiada argumentów.
+        {
+            a = b = c = 1.0;
+        }
+
+        public Trojkat(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+    }
+}
